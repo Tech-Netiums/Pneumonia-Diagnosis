bed93588e4c2452af01a419c769d6cf2614bd23d
Chaaaarles <charles.gaillard@eleves.enpc.fr>
2020-05-04 20:56:10 +0200

    custom_network + plot_utils

---
+++ plot_utils.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#FONCTION POUR AFFICHER DES IMAGES DU DATASET
+
+def show(images, n_cols=None):
+    n_cols = n_cols or len(images)
+    n_rows = (len(images) - 1) // n_cols + 1
+    if images.shape[-1] == 1:
+        images = np.squeeze(images, axis=-1)
+    plt.figure(figsize=(n_cols, n_rows))
+    for index, image in enumerate(images):
+        plt.subplot(n_rows, n_cols, index + 1)
+        plt.imshow(image, cmap="binary")
+        plt.axis("off")
+    plt.show()
